60f85a0aa10a80af44193212a4920f4225b77f90
Jaana Mäll <jaana.mall@hotmail.com>
2022-02-27 16:16:34 +0200

    preparation for testing

---
+++ Trainer.UnitTests/ControllerTests/ClientControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trainer.Core.Repository.ClientRepo;
+using Trainer.Controllers;
+using Xunit;
+using Trainer.Core.Repository;
+using Trainer.Models;
+using Trainer.Core.Repository.ExerciseRepo;
+
+namespace Trainer.UnitTests.ControllerTests
+{
+    public class ClientControllerTests
+    {
+        private readonly Mock<IBaseRepository<Client>> _clientRepositoryMock;
+        private readonly ClientsController _clientsController;
+
+        public ClientControllerTests()
+        {
+            _clientRepositoryMock = new Mock<IBaseRepository<Client>>();
+            //_clientsController = new ClientsController(_clientRepositoryMock.Object);
+        }
+        
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Trainer.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Trainer.Controllers;
+
+namespace Trainer.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
